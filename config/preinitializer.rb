5d2a72ebde55db0995ad018cb7dbbd4816cab639
Pawel <pavel.jarema@gmail.com>
2015-04-09 19:34:39 +0200

    config

--- config/preinitializer.rb
+++ config/preinitializer.rb
@@ -1,6 +1,6 @@
 require 'active_support/core_ext'
 require 'konf'
 
-# pub_config = (YAML.load(ERB.new(File.read(File.expand_path('../config.yml',     __FILE__))).result)[Rails.env]) || {}
-# AppConfig = Konf.new(pub_config)
+pub_config = (YAML.load(ERB.new(File.read(File.expand_path('../config.yml',     __FILE__))).result)[Rails.env]) || {}
+AppConfig = Konf.new(pub_config)
 
